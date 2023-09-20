94c447d4ad00f6852b5a67b9bac4803f2b847aad
eons-shubham <73813004+eons-shubham@users.noreply.github.com>
2023-09-20 07:39:23 +0000

    local vs instance variable

--- OOPs.java
+++ OOPs.java
@@ -4,6 +4,7 @@ public class OOPs{
     }
 }
 
+
 class singleton{
 
     static singleton singletonClassObject = new singleton();
@@ -15,4 +16,23 @@ class singleton{
     static singleton getter(){
         return singletonClassObject;
     }
+}
+
+//instance variables vs local variables
+
+class instanceVariables{
+
+    // the variables name and age will be available to all function/methods of this class
+    String name;
+    int age;
+
+    // the variables localName and localAge are restricted to the setter method scope only
+    void setter(String localName, int localAge){
+        // some code
+    }
+
+    void getter(){
+        // some code
+    }
+
 }
